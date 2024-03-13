28e6c186771ca40c2277de669d93e8f0fa7e3f0e
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-03-13 12:37:21 +0600

    show country information on display and responsive for mobiel & tablet devices

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -11,6 +13,14 @@ const Countries = () => {
     return (
         <div>
            <h2>Countries {countries.length}</h2> 
+           <div className="country-container">
+           {
+            countries.map(country => <Country
+                 key={country.cca2}
+                country={country}>
+                </Country>)
+           }
+           </div>
         </div>
     );
 };
